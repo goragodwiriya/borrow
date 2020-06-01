fb8330c839510075ca5daaa1a6eb19f607c6144e
Goragod <admin@goragod.com>
2020-06-01 08:37:44 +0700

    update card

--- modules/borrow/controllers/home.php
+++ modules/borrow/controllers/home.php
@@ -33,11 +33,11 @@ class Controller extends \Gcms\Controller
     public static function addCard(Request $request, $card, $login)
     {
         $items = \Borrow\Home\Model::get($login);
-        \Index\Home\Controller::renderCard($card, 'icon-exchange', '{LNG_Borrow} &amp; {LNG_Return}', number_format($items->pending), Language::find('BORROW_STATUS', null, 0), 'index.php?module=borrow-setup&amp;status=0');
-        \Index\Home\Controller::renderCard($card, 'icon-valid', '{LNG_Borrow} &amp; {LNG_Return}', number_format($items->confirmed), Language::find('BORROW_STATUS', null, 2), 'index.php?module=borrow-setup&amp;status=2');
-        \Index\Home\Controller::renderCard($card, 'icon-warning', '{LNG_Borrow} &amp; {LNG_Return}', number_format($items->returned), '{LNG_Un-Returned items}', 'index.php?module=borrow-setup&amp;status=2&amp;due=1');
+        \Index\Home\Controller::renderCard($card, 'icon-exchange', '{LNG_Borrow} &amp; {LNG_Return} ('.$login['name'].')', number_format($items->pending), Language::find('BORROW_STATUS', null, 0), 'index.php?module=borrow-setup&amp;status=0');
+        \Index\Home\Controller::renderCard($card, 'icon-valid', '{LNG_Borrow} &amp; {LNG_Return} ('.$login['name'].')', number_format($items->confirmed), Language::find('BORROW_STATUS', null, 2), 'index.php?module=borrow-setup&amp;status=2');
+        \Index\Home\Controller::renderCard($card, 'icon-warning', '{LNG_Borrow} &amp; {LNG_Return} ('.$login['name'].')', number_format($items->returned), '{LNG_Un-Returned items}', 'index.php?module=borrow-setup&amp;status=2&amp;due=1');
         if (isset($items->allpending)) {
-            \Index\Home\Controller::renderCard($card, 'icon-exchange', '{LNG_Borrow} &amp; {LNG_Return}', number_format($items->allpending), '{LNG_Waiting list}', 'index.php?module=borrow-report&amp;status=0');
+            \Index\Home\Controller::renderCard($card, 'icon-exchange', '{LNG_Borrow} &amp; {LNG_Return} ({LNG_Can be approve})', number_format($items->allpending), '{LNG_Waiting list}', 'index.php?module=borrow-report&amp;status=0');
         }
     }
 }
